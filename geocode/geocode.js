3499efe39ddafc8fa72315f6b24a28452176b9d9
Rick Thornbrugh <Rick Thornbrugh>
2017-02-07 20:58:58 -0800

    refactor and incorporate callback function.

--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,22 +1,22 @@
 const request = require('request');
 
-var geocodeAddress = (address) => {
+var geocodeAddress = (address, callback) => {
   var encodedAddress = encodeURIComponent(address);
   request({
     url: `http://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
   }, (error, response, body) => {
     if(error){
-      console.log('Unable to connect to Google servers.');
+      callback('Unable to connect to Google servers.')
     } else if(body.status === 'ZERO_RESULTS'){//body.status is specific to Google
-      console.log('Address info entered returned no results.');
+      callback('Address info entered returned no results.');
     } else if(body.status === 'OK'){
-      console.log(`Address: ${body.results[0].formatted_address}`);
-      console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
-      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-    } else {
-      console.log('Unable to process request');
-    }
+      callback(undefined, {
+        address: body.results[0].formatted_address,
+        lattitude: body.results[0].geometry.location.lat,
+        longitude: body.results[0].geometry.location.lng
+      });
+    } 
   });
 };
 
